9b1a5a8a862a53c0bd46fd155387592933663b79
Yash Yerunkar <yashyerunkar8@gmail.com>
2024-08-25 17:33:25 +0530

    feat:

--- app/(root)/careers/page.tsx
+++ app/(root)/careers/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return <div>Work page</div>;
+  return <div className="text-center mt-20">Careers Page</div>;
 };
 
 export default page;
---
+++ app/(root)/clients/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div className="text-center mt-20">Clients Page</div>;
+};
+
+export default page;
